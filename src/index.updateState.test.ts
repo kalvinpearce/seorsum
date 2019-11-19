b34fac0f67d8be3fc641b563cc1c38994112fe2b
Kalvin Pearce <kalvinpearce@gmail.com>
2019-11-19 11:46:07 +1000

    Clean up updateState tests

--- src/index.updateState.test.ts
+++ src/index.updateState.test.ts
@@ -10,24 +10,36 @@ const initialState = {
 
 const newA = 'world';
 const newFirstName = 'james';
+
 describe('updateState', () => {
   it('updates shallow state', () => {
+    /* Setup */
     const { updateState } = createStore(initialState);
+
+    /* Action */
     const newState = updateState(d => {
       d.a = newA;
     });
+
+    /* Test */
     expect(newState.a).toBe(newA);
   });
 
   it('updates deep state', () => {
+    /* Setup */
     const { updateState } = createStore(initialState);
+
+    /* Action */
     const newState = updateState(d => {
       d.name.first = newFirstName;
     });
+
+    /* Test */
     expect(newState.name.first).toBe(newFirstName);
   });
 
   it('updates state from async functions', async () => {
+    /* Setup */
     const { updateState } = createStore(initialState);
     const asyncAction = async () => {
       await new Promise(resolve => setTimeout(resolve, 100));
@@ -36,15 +48,23 @@ describe('updateState', () => {
       });
     };
 
+    /* Action */
     const newState = await asyncAction();
+
+    /* Test */
     expect(newState.a).toBe(newA);
   });
 
   it("doesn't update the wrong state", () => {
+    /* Setup */
     const { updateState } = createStore(initialState);
+
+    /* Action */
     const newState = updateState(d => {
       d.name.first = newFirstName;
     });
+
+    /* Test */
     expect(newState.a).toBe(initialState.a);
   });
 });
